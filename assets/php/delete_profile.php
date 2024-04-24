989a1802a9de74ab658b53617d302ce24da65a43
LeenAlshehri2003 <miss.leen.2704@gmail.com>
2024-04-24 09:02:02 +0300

    Leen

--- assets/php/delete_profile.php
+++ assets/php/delete_profile.php
@@ -15,7 +15,7 @@ if (isset($_GET['profile_id'], $_GET['profile_type'])) {
         echo "<script>alert('Profile deleted successfully.'); window.location.href = '/main_page.php';</script>";
     } else {
         // Determine the profile page based on the type of profile
-        $errorRedirectPage = ($profile_type === 'learner') ? '/profile_page_learner.php' : '/profile_page_partner.php';
+        $errorRedirectPage = ($profile_type === 'learner') ? '../HTML pages/ProfilePage-LanguageLearner.php' : '../HTML pages/ProfilePage-LanguagePartner.php';
         echo "<script>alert('Error deleting profile.'); window.location.href = '$errorRedirectPage';</script>";
     }
 }
