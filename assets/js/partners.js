d5e1054f43689202e232ad3bc2d6a9958bc3089b
Danah970 <danah.9700@gmail.com>
2024-05-01 14:20:41 +0300

    danah

--- assets/js/partners.js
+++ assets/js/partners.js
@@ -76,7 +76,7 @@ function displayPartners(partners) {
                             ${partner.Languages.split(', ').map(function(language) {
                                 return `<span>${language}</span>`;
                             }).join('')}
-                            <a class="f-right" href="#"><img src="${partner.Photo}" width="85" height="85" alt="Partner picture"></a>
+                            <a class="f-right" href="#"><img src=<?php echo htmlspecialchars('../assets/img/Partners images/' .${partner.Photo}); ?>" width="85" height="85" alt="Partner picture"></a>
                         </div>
                         <h4 class="sub-title mb-20"><a class="partner-link" data-partner-id="${partner.PartnerID}" href="#">${partner.FullName}</a></h4>
                         <div class="course__meta">
