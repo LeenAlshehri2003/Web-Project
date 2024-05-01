dddd957c6e2f8c39e91b548967a3ab63f0703241
Danah970 <danah.9700@gmail.com>
2024-05-01 17:07:24 +0300

    danah

--- assets/php/fetchReviews.php
+++ assets/php/fetchReviews.php
@@ -3,9 +3,9 @@ require_once 'db.php';  // Ensure this points to your actual database connection
 session_start();
 
 //Ensure the user is logged in
-//if (!isset($_SESSION['user_id'])) {
-  //  exit('User not logged in.');  // Proper handling for not logged-in users
-//}
+if (!isset($_SESSION['user_id'])) {
+    exit('User not logged in.');  // Proper handling for not logged-in users
+}
 $userId = $_SESSION['user_id']; 
 
 
