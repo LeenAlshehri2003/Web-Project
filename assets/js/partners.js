101943cb879b7866930b4c5d47dc50ba97106478
Danah970 <danah.9700@gmail.com>
2024-05-06 14:01:15 +0300

    danah

--- assets/js/partners.js
+++ assets/js/partners.js
@@ -69,7 +69,7 @@ function displayPartners(partners) {
                     <div class="z-gallery__thumb mb-20">
                         <a href="#"><img class="img-fluid" src="../assets/img/Partners images/${partner.Photo}" alt="" width="500" height="500"></a>
                         <div class="feedback-tag">${partner.AverageRating}</div>
-                        <div class="heart-icon"><i class="fas fa-heart"></i></div>
+                  
                     </div>
                     <div class="z-gallery__content">
                         <div class="course__tag mb-15">
