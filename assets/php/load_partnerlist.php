fbd6e0c5f477a49423377d6fda50e8e9a087f8de
Danah970 <danah.9700@gmail.com>
2024-05-06 22:24:39 +0300

    danah

--- assets/php/load_partnerlist.php
+++ assets/php/load_partnerlist.php
@@ -38,9 +38,6 @@ GROUP BY
 ORDER BY 
     AverageRating DESC;");
 
-
-
-
 $sqlPartners->execute();
 
 // Fetch data
@@ -50,32 +47,57 @@ $result = $sqlPartners->get_result();
 if ($result->num_rows > 0) {
     // Loop through each partner and display profile details
     while ($row = $result->fetch_assoc()) {
+        $languageClasses = ''; // Initialize variable to store language classes
+        $languageIDs = explode(', ', $row['Languages']); // Split languages into an array
+        foreach ($languageIDs as $languageID) {
+            // Append language class based on language ID
+            switch ($languageID) {
+                case "1":
+                    $languageClasses .= " cat1"; // English
+                    break;
+                case "2":
+                    $languageClasses .= " cat2"; // Arabic
+                    break;
+                case "3":
+                    $languageClasses .= " cat3"; // French
+                    break;
+                case "4":
+                    $languageClasses .= " cat4"; // Spanish
+                    break;
+                case "5":
+                    $languageClasses .= " cat5"; // Italian
+                    break;
+                case "6":
+                    $languageClasses .= " cat6"; // Japanese
+                    break;
+                case "7":
+                    $languageClasses .= " cat7"; // Chinese
+                    break;
+                // Add more cases for other languages as needed
+                default:
+                    break;
+            }
+        }
         ?>
-        <div class="grid-item">
-        <div class="z-gallery mb-30">
-            <div class="z-gallery__thumb mb-20">
-                <a href="#"><img class="img-fluid" src="../assets/img/Partners images/<?php echo $row['ProfilePicture']; ?>" alt="" width="500" height="500"></a>
-                <!-- Display AverageRating -->
-                <div class="feedback-tag"><?php echo $row['AverageRating']; ?></div>
+        <div class="grid-item<?php echo $languageClasses; ?>"> <!-- Add language classes to grid item -->
+            <div class="z-gallery mb-30">
+                <div class="z-gallery__thumb mb-20">
+                    <a href="#"><img class="img-fluid" src="../assets/img/Partners images/<?php echo $row['ProfilePicture']; ?>" alt="" width="500" height="500"></a>
+                    <!-- Display AverageRating -->
+                    <div class="feedback-tag"><?php echo $row['AverageRating']; ?></div>
+                </div>
+                <div class="z-gallery__content">
+                    <div class="course__tag mb-15">
+                        <?php echo $row['Languages']; ?> <!-- Display languages -->
+                    </div>
+                    <h4 class="sub-title mb-20"><a class="partner-link" data-partner-id="<?php echo $row['PartnerID']; ?>" href="#"><?php echo $row['FullName']; ?></a></h4>
+                    <div class="course__meta">
+                        <span><?php echo $row['Bio']; ?></span>
+                    </div>
+                    <br>
+                    <h6><?php echo $row['SessionPrice']; ?>$/hour</h6>
+                </div>
             </div>
-            <div class="z-gallery__content">
-                <div class="course__tag mb-15">
-                    <?php
-                    // Convert the comma-separated languages string into an array
-                    $languages = explode(', ', $row['Languages']);
-                    foreach ($languages as $language) {
-                        echo "<span>$language</span>";
-                    }
-                    ?>
-                </div>
-                <h4 class="sub-title mb-20"><a class="partner-link" data-partner-id="<?php echo $row['PartnerID']; ?>" href="#"><?php echo $row['FullName']; ?></a></h4>
-                <div class="course__meta">
-                    <span><?php echo $row['Bio']; ?></span>
-                </div>
-                <br>
-                <h6><?php echo $row['SessionPrice']; ?>$/hour</h6>
-            </div>
-                </div>
         </div>
         <?php
     }
@@ -83,5 +105,3 @@ if ($result->num_rows > 0) {
     echo "No partners found.";
 }
 // Close the prepared statement
-$sqlPartners->close();
-?>
